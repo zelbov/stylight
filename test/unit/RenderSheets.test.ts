1d532e91e164cacf9fec559557763ade0c666d5e
Zelbov <jimborecard@gmail.com>
2022-12-08 09:08:38 +0200

    Add nested styles test

--- test/unit/RenderSheets.test.ts
+++ test/unit/RenderSheets.test.ts
@@ -11,4 +11,19 @@ describe('Style rendering unit testing', () => {
 
     })
 
+    it('Render nested styles: should return valid string with nested style rules', () => {
+
+        const rendered = renderStyleSheet({
+            menu: {
+                border: '1px solid #000',
+                '& div': { backgroundColor: 'red' }
+            }
+        })
+        
+        expect(rendered).contain('.menu {border:1px solid #000;}')
+        expect(rendered).contain('.menu div {background-color:red;}')
+
+
+    })
+
 })
