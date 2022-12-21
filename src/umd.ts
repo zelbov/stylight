3f1931e0137ea0b559e30ba6d3750c1e95c24ae2
Zelbov <jimborecard@gmail.com>
2022-12-21 20:44:11 +0200

    Added missing UMD package exports: `explainStylesheet`, `renderExplained` plus removed UMD entrypoints from node target compilation tree

--- src/umd.ts
+++ src/umd.ts
@@ -1,7 +1,7 @@
-import { createTheme, renderStyleSheet, styledClass } from "."
+import { createTheme, explainStyleSheet, renderExplained, renderStyleSheet, styledClass } from "."
 
 // require
-module.exports = { renderStyleSheet, styledClass, createTheme }
+module.exports = { renderStyleSheet, styledClass, createTheme, explainStyleSheet, renderExplained }
 
 //@ts-ignore
 global.Stylight = module.exports // UMD
