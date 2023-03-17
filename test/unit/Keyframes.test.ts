26ef8a3908cf82d4a06d97b941f75fe046cebf7c
Zelbov <jimborecard@gmail.com>
2023-03-17 09:53:47 +0200

    Add tests for keyframes inside nested target

--- test/unit/Keyframes.test.ts
+++ test/unit/Keyframes.test.ts
@@ -80,4 +80,88 @@ describe('Keyframes rule unit testing', () => {
 
     })
 
+    describe('Nested selector scoped keyframes', () => {
+
+        it('Define keyframes rule with from/to: should pass typechecks & produce valid output', () => {
+
+            const { render } = createStyleSheet({
+
+                foo: {
+
+                    '& div': {
+
+                        animationName: 'animateFoo',
+                        animationDuration: '.5s',
+
+                        keyframes: [{
+
+                            name: 'animateFoo',
+                            steps: [{
+                                from: { background: '#fff' },
+                                to: { background: '#000' }
+                            }]
+        
+                        }],
+
+                    }
+
+                }
+
+            })
+
+            const result = render()
+
+            console.log(result)
+
+            expect(result).contain('@keyframes animateFoo')
+            expect(result).contain('from {background:#fff}')
+            expect(result).contain('to {background:#000}')
+
+        })
+        it('Define keyframes rule with percentage steps: should pass typechecks & produce valid output', () => {{
+
+            const { render } = createStyleSheet({
+
+                foo: {
+
+                    '& div': {
+
+                        animationName: 'animateFoo',
+                        animationDuration: '.5s',
+
+                        keyframes: [{
+
+                            name: 'animateFoo',
+                            steps: [{
+                                percentage: '25',
+                                css: {
+                                    background: '#00aaff'
+                                }
+                            }, {
+                                percentage: '50',
+                                css: {
+                                    background: '#00aa00'
+                                }
+                            }]
+        
+                        }],
+
+                    }                    
+
+                }
+
+            })
+
+            const result = render()
+
+            console.log(result)
+
+            expect(result).contain('@keyframes animateFoo')
+            expect(result).contain('25% {background:#00aaff}')
+            expect(result).contain('50% {background:#00aa00}')
+
+        }})
+
+    })
+
 })
