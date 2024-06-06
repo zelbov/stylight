f14b0a1a7aef180c726d533b82a45da496dee2cb
Zelbov <jimborecard@gmail.com>
2024-06-06 21:44:17 +0300

    Fix `btoa` deprecation notice

--- src/common/Seed.ts
+++ src/common/Seed.ts
@@ -5,4 +5,5 @@ export type SeedStringOrFunction = string | SeedFunction
 
 export const DEFAULT_SEED_FUNCTION : SeedFunction = (target: string) => target
 
-export const assignSeedString = (seed: string) => (target: string) => btoa(seed+target)
+export const assignSeedString = (seed: string) => (target: string) => 
+    Buffer ? Buffer.from(seed+target).toString('base64') : window ? window.btoa(seed+target) : seed+target
